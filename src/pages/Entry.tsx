b2a70f8fd35bad3befd2b128969ec357a95990cf
211250164 <211250164@smail.nju.edu.cn>
2023-09-18 09:33:02 +0800

    fix: 修复import报错

--- src/pages/Entry.tsx
+++ src/pages/Entry.tsx
@@ -1,20 +1,19 @@
-import { Button, Menu, MenuProps } from 'antd'
-import Search, { SearchProps } from 'antd/es/input/Search'
+import { Button, Menu, MenuProps, Input, Image } from 'antd'
 import { useRouter } from 'next/router'
 import React from 'react'
 
 const items = [
   {
     label: '首页',
-    key: 'MainPage',
+    key: '/MainPage',
   },
   {
     label: '查学校',
-    key: 'SearchSchoolPage',
+    key: '/SearchSchoolPage',
   },
   {
     label: '查专业',
-    key: 'SearchMajorPage',
+    key: '/SearchMajorPage',
   },
 ]
 
@@ -25,12 +24,47 @@ export default function Entry({ children }: { children: React.ReactNode }) {
     setCurrent(e.key)
     router.push(e.key)
   }
-  const onSearch: SearchProps['onSearch'] = (value, _e, info) =>
-    console.log(info?.source, value)
+  const onSearch = (value: any) => console.log(value)
   return (
     <>
-      <div>
-        <Search
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-around',
+          alignItems: 'center',
+          height: 100,
+        }}
+      >
+        <div>
+          <Image
+            style={{
+              cursor: 'pointer',
+              width: 169,
+              marginRight: 10,
+            }}
+            src={'https://www.gaokao.cn/static/media/head_logo1.133254dc.png'}
+            preview={false}
+            onClick={() => (window.location.href = 'https://www.eol.cn/')}
+          />
+          <Image
+            style={{
+              cursor: 'pointer',
+              width: 169,
+              marginRight: 10,
+            }}
+            src={
+              'https://t1.chei.com.cn/gaokao/images/index/gk_logo.png?v=1620294265752'
+            }
+            preview={false}
+            onClick={() =>
+              (window.location.href = 'https://gaokao.chsi.com.cn/')
+            }
+          />
+        </div>
+        <Input.Search
+          style={{
+            width: '30%',
+          }}
           placeholder="查大学，查专业，搜问答"
           allowClear
           size="large"
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,10 @@
-import { Menu, MenuProps } from 'antd'
-import { useRouter } from 'next/router'
 import React from 'react'
-import MainPage from './MainPage'
 import Entry from './Entry'
 
 export default function Home() {
   return (
     <>
-      <Entry></Entry>
+      <Entry> </Entry>
     </>
   )
 }
