1accdf0cf647f2101ad77c1f311fa4d58b6901f2
Lenovo <Lenovo@Lenovo.com>
2023-09-18 21:04:55 +0800

    feat: 添加看一看页，实现简单的响应式设计

--- src/pages/VideoPlayPage.tsx
+++ src/pages/VideoPlayPage.tsx
@@ -1,9 +1,88 @@
+// 导入React和antd的组件
+import React from 'react'
+import { Card, Image, Avatar } from 'antd'
 import Entry from '../components/Entry'
+import styles from './VideoPlayPage.module.css'
+const { Meta } = Card
+
+function CardItem(props) {
+  const { title, description, image, avatar } = props
+  return (
+    <Card
+      className={styles.card}
+      hoverable
+      cover={<Image preview={false} alt="example" src={image} />}
+    >
+      <Meta
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+        }}
+        avatar={<Avatar src={avatar} />}
+        title={title}
+        description={description}
+      />
+    </Card>
+  )
+}
 
 export default function VideoPlayPage() {
+  const cardItems = cards.map(card => <CardItem key={card.title} {...card} />)
   return (
     <>
-      <Entry>This is the VideoPlayPage</Entry>
+      <Entry>
+        <div className={styles.cardContainer}>{cardItems}</div>
+      </Entry>
     </>
   )
 }
+const cards = [
+  {
+    title: 'Card title 1',
+    description: 'This is the description 1',
+    image: 'https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png',
+    avatar: 'https://xsgames.co/randomusers/avatar.php?g=pixel&key=2',
+  },
+  {
+    title: 'Card title 2',
+    description: 'This is the description 2',
+    image: 'https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png',
+    avatar: 'https://xsgames.co/randomusers/avatar.php?g=pixel&key=3',
+  },
+  {
+    title: 'Card title 3',
+    description: 'This is the description 3',
+    image: 'https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png',
+    avatar: 'https://xsgames.co/randomusers/avatar.php?g=pixel&key=4',
+  },
+  {
+    title: 'Card title 3',
+    description: 'This is the description 3',
+    image: 'https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png',
+    avatar: 'https://xsgames.co/randomusers/avatar.php?g=pixel&key=4',
+  },
+  {
+    title: 'Card title 3',
+    description: 'This is the description 3',
+    image: 'https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png',
+    avatar: 'https://xsgames.co/randomusers/avatar.php?g=pixel&key=4',
+  },
+  {
+    title: 'Card title 3',
+    description: 'This is the description 3',
+    image: 'https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png',
+    avatar: 'https://xsgames.co/randomusers/avatar.php?g=pixel&key=4',
+  },
+  {
+    title: 'Card title 3',
+    description: 'This is the description 3',
+    image: 'https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png',
+    avatar: 'https://xsgames.co/randomusers/avatar.php?g=pixel&key=4',
+  },
+  {
+    title: 'Card title 3',
+    description: 'This is the description 3',
+    image: 'https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png',
+    avatar: 'https://xsgames.co/randomusers/avatar.php?g=pixel&key=4',
+  },
+]
