051dc6bd7b3b93fd11ec56716a9edfb42ac7b952
211250165 <211250165@smail.nju.edu.cn>
2023-11-06 22:05:00 +0800

    update: try beautifying and adding data

--- src/components/EchartsMap.jsx
+++ src/components/EchartsMap.jsx
@@ -16,6 +16,25 @@ function EChartsMap() {
                     roam: false,// 一定要关闭拖拽
                     zoom: 1.23,
                     center: [105, 36], // 调整地图位置
+                    itemStyle: {
+                        normal: {
+                            areaColor: "#F0FFFF",
+                            borderColor: "#1677FF",
+                            borderWidth: 1, //设置外层边框
+                            shadowBlur: 8,
+                            shadowOffsetY: 8,
+                            shadowOffsetX: 0,
+                            shadowColor: "#87CEFA"
+                        },
+                        emphasis: {
+                            areaColor: "#F5FFFA",
+                            shadowOffsetX: 0,
+                            shadowOffsetY: 0,
+                            shadowBlur: 5,
+                            borderWidth: 0,
+                            shadowColor: "rgba(0, 0, 0, 0.5)"
+                        }
+                    }
                 },
                 // series: [
                 //     {
@@ -26,23 +45,24 @@ function EChartsMap() {
             };
 
             myChart.setOption(option);
-        } else{
+        } else {
             console.log("faild to init chartRef")
         }
     };
 
     useEffect(() => {
 
-        fetch('/files/ChinaMap.geoJson')
+        fetch('/files/blogd8fd6f7912ba2533e02c0b1a47d4a626.json')
             .then(response => response.json())
             .then(data => {
                 setFeatures(data)
+                console.log(data)
             })
             .catch(error => {
                 console.error(error)
             })
 
-        
+
     }, []);
 
     if (features) {
@@ -52,7 +72,7 @@ function EChartsMap() {
     return (
         <div
             ref={chartRef}
-            style={{  width:"500px", height: "500px" }}
+            style={{ width: "500px", height: "500px" }}
         >Loading...</div>
     );
 }
