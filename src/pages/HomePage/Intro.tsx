dbe8f668d48591279b4626c61a2c05fc3911472e
211250164 <211250164@smail.nju.edu.cn>
2023-12-05 15:28:42 +0800

    style:merge和格式化

--- src/pages/HomePage/Intro.tsx
+++ src/pages/HomePage/Intro.tsx
@@ -1,72 +1,89 @@
 import React from 'react'
-import {ActiveDraggableContext} from "@dnd-kit/core/dist/components/DndContext";
+import { ActiveDraggableContext } from '@dnd-kit/core/dist/components/DndContext'
 
 const Intro: React.FC = () => {
-    return (
-
-        <div style={{
-            boxSizing: 'border-box',
-            backgroundColor: "#fff",
-            marginTop: 100,
-            marginBottom: 100,
-        }}>
-            <div style={{
-                boxSizing: 'border-box',
-                width: '100%',
-                paddingTop: '64px',
-            }}></div>
-            <div style={{
-                margin: '0 auto',
-                width: '100%',
-                fontSize: 30,
-                fontWeight: 700,
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: "center"
-            }}>Go School</div>
-            <div style={{
-                boxSizing: 'border-box',
-                margin: '0 auto',
-                width: '100%',
-                maxWidth: '1349px',
-                padding: '0 24px',
-            }}>
-                <div style={{
-                    width: '91%',
-                    display: 'flex',
-                    position: 'relative',
-                    alignItems: 'center',
-                    flexDirection: 'column',
-                    flexWrap: 'wrap',
-                    paddingLeft: '32px',
-                }}>
-                    <h1 style={{
-                        direction: 'ltr',
-                        paddingTop: 30,
-                        paddingLeft: 30,
-                        width: "56%",
-                        fontFamily: 'Helvetica, Arial, sans-serif',
-                        fontWeight: 'normal',
-                        lineHeight: '120%',
-                        margin: 0,
-                        textRendering: 'optimizeLegibility',
-                        color: '#1c2b33',
-                        fontSize: '24px',
-                        letterSpacing: '.005em',
-                        textAlign: 'center',
-                        marginBottom: '16px',
-                    }}>
-                        GoSchool provides high school graduates with a search function for universities and their majors across China, as well as a style display.
-                    </h1>
-                </div>
-            </div>
-            <div style={{
-                boxSizing: 'border-box',
-                width: '100%',
-                paddingTop: '80px',
-            }}></div>
+  return (
+    <div
+      style={{
+        boxSizing: 'border-box',
+        backgroundColor: '#fff',
+        marginTop: 100,
+        marginBottom: 100,
+      }}
+    >
+      <div
+        style={{
+          boxSizing: 'border-box',
+          width: '100%',
+          paddingTop: '64px',
+        }}
+      ></div>
+      <div
+        style={{
+          margin: '0 auto',
+          width: '100%',
+          fontSize: 30,
+          fontWeight: 700,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        Go School
+      </div>
+      <div
+        style={{
+          boxSizing: 'border-box',
+          margin: '0 auto',
+          width: '100%',
+          maxWidth: '1349px',
+          padding: '0 24px',
+        }}
+      >
+        <div
+          style={{
+            width: '91%',
+            display: 'flex',
+            position: 'relative',
+            alignItems: 'center',
+            flexDirection: 'column',
+            flexWrap: 'wrap',
+            paddingLeft: '32px',
+          }}
+        >
+          <h1
+            style={{
+              direction: 'ltr',
+              paddingTop: 30,
+              paddingLeft: 30,
+              width: '56%',
+              fontFamily: 'Helvetica, Arial, sans-serif',
+              fontWeight: 'normal',
+              lineHeight: '120%',
+              margin: 0,
+              textRendering: 'optimizeLegibility',
+              color: '#1c2b33',
+              fontSize: '24px',
+              letterSpacing: '.005em',
+              textAlign: 'center',
+              marginBottom: '16px',
+            }}
+          >
+            GoSchool provides high school graduates with a search function for
+            universities and their majors across China, as well as a style
+            display.
+          </h1>
         </div>
-    )
+      </div>
+      <div
+        style={{
+          boxSizing: 'border-box',
+          width: '100%',
+          paddingTop: '80px',
+        }}
+      ></div>
+    </div>
+  )
 }
 
 export default Intro
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,28 +1,35 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import Entry from '../components/Entry'
 import Slider from '@/pages/HomePage/Slider'
 import SmartRecommend from '@/pages/HomePage/SmartRecommend'
 import MonthCalendar from '@/pages/HomePage/MonthCalendar'
 import PolicyInformation from '@/pages/HomePage/PolicyInformation'
 import CommonSense from '@/pages/HomePage/CommonSense'
-import Intro from "@/pages/HomePage/Intro"
-import IndexHeader from "@/pages/HomePage/IndexHeader"
-import QueueAnim from "rc-queue-anim";
+import Intro from '@/pages/HomePage/Intro'
+import IndexHeader from '@/pages/HomePage/IndexHeader'
+import QueueAnim from 'rc-queue-anim'
 
-
-const Mytest:React.FC = () => {
+const Mytest: React.FC = () => {
   const [isClient, setIsClient] = useState(false)
 
   useEffect(() => {
     setIsClient(true)
   }, [])
 
-  return <>{isClient?<QueueAnim>
-    <div key="demo1">依次进场</div>
-    <div key="demo2">依次进场</div>
-    <div key="demo3">依次进场</div>
-    <div key="demo4">依次进场</div>
-  </QueueAnim>:<></>}</>
+  return (
+    <>
+      {isClient ? (
+        <QueueAnim>
+          <div key="demo1">依次进场</div>
+          <div key="demo2">依次进场</div>
+          <div key="demo3">依次进场</div>
+          <div key="demo4">依次进场</div>
+        </QueueAnim>
+      ) : (
+        <></>
+      )}
+    </>
+  )
 }
 
 export default function Home() {
@@ -33,13 +40,12 @@ export default function Home() {
     alignItems: 'center',
   }
 
-
   return (
     <>
       <Entry>
         <div>
-            {/*<Mytest></Mytest>*/}
-            <IndexHeader></IndexHeader>
+          {/*<Mytest></Mytest>*/}
+          <IndexHeader></IndexHeader>
           {/*<div style={headerContainer}>*/}
           {/*  <MonthCalendar></MonthCalendar>*/}
           {/*  <Slider></Slider>*/}
@@ -53,5 +59,3 @@ export default function Home() {
     </>
   )
 }
-
-
