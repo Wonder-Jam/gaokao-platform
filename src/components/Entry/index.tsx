fc25c9f4c6c3dbab276bfa9908427bc98ebc9345
211250164 <211250164@smail.nju.edu.cn>
2023-12-18 21:52:29 +0800

    bugfix: 删除登录注册按钮

--- src/components/Entry/index.tsx
+++ src/components/Entry/index.tsx
@@ -1,15 +1,6 @@
 import { usePageNavigation } from '../../hooks/usePageNavigation'
 import logo from '../../static/logo.jpeg'
-import {
-  Button,
-  Menu,
-  MenuProps,
-  Input,
-  Image,
-  Modal,
-  Form,
-  Select,
-} from 'antd'
+import { Menu, MenuProps, Image } from 'antd'
 import { useRouter } from 'next/router'
 import React from 'react'
 import {
@@ -83,17 +74,11 @@ function GaoKaoMenu() {
 
 function HeaderBar() {
   const { goToEolPage, goToYangGuangGaoKaoPage } = usePageNavigation()
-  const [isModalOpen, setIsModalOpen] = React.useState(false)
-  const openLoginModal = () => setIsModalOpen(true)
-  const closeLoginModal = () => setIsModalOpen(false)
   return (
     <HeaderBarContainer>
       <div>
         <ImageContainer>
           <Image
-            // src={
-            //   'files/blog218cc985c57906433217d46ca1db1205.png'
-            // }
             src={logo.src}
             preview={false}
             // style={{ transform: 'scale(0.6)' }}
@@ -112,88 +97,6 @@ function HeaderBar() {
         </ImageContainer> */}
       </div>
       <GaoKaoMenu />
-      {/* <Input.Search
-        style={{
-          width: '15%',
-          minWidth: 190,
-        }}
-        placeholder="查大学，查专业，搜问答"
-        allowClear
-        size="large"
-      /> */}
-      <div
-        style={{
-          width: '15%',
-        }}
-      >
-        <Button onClick={openLoginModal}>登录 | 注册</Button>
-      </div>
-      <LoginModal isModalOpen={isModalOpen} closeLoginModal={closeLoginModal} />
     </HeaderBarContainer>
-  )
-}
-
-function LoginModal({
-  isModalOpen,
-  closeLoginModal,
-}: {
-  isModalOpen: boolean
-  closeLoginModal: () => void
-}) {
-  const prefixSelector = (
-    <Form.Item name="prefix" noStyle>
-      <Select
-        defaultValue="86"
-        options={[
-          { value: '86', label: '+86' },
-          { value: '87', label: '+87' },
-        ]}
-        style={{ width: 70 }}
-      />
-    </Form.Item>
-  )
-  return (
-    <>
-      <Modal
-        open={isModalOpen}
-        onCancel={closeLoginModal}
-        title="登录畅想更多权益"
-        footer={null}
-        centered
-        maskClosable
-        destroyOnClose
-      >
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            height: 200,
-            marginTop: 24,
-          }}
-        >
-          <Form.Item
-            name="phone"
-            rules={[
-              {
-                message: '请输入手机号',
-              },
-            ]}
-          >
-            <Input placeholder="请输入手机号" addonBefore={prefixSelector} />
-          </Form.Item>
-          <Form.Item name="validation">
-            <Input placeholder="请输入验证码" suffix="获取验证码" />
-          </Form.Item>
-          <div
-            style={{
-              display: 'flex',
-              justifyContent: 'end',
-            }}
-          >
-            账号密码登录
-          </div>
-        </div>
-      </Modal>
-    </>
   )
 }
